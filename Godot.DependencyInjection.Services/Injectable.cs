4882a46216b2618e95bea30a975a2417d2823e07
Filip Drabinski <58997858+Filip-Drabinski@users.noreply.github.com>
2025-04-22 00:25:04 +0200

    Add multiple additional wrappers for Godot static classes

---
+++ Godot.DependencyInjection.Services/Injectable.cs
@@ -0,0 +1,21 @@
+﻿[assembly: Fluentish.InjectableStatic.InjectableStaticConfiguration(
+    Namespace = "Fluentish.Godot.DependencyInjection.Services",
+    NamespaceMode = Fluentish.InjectableStatic.NamespaceMode.Const
+)]
+
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.GD))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.Input))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.Engine))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.Performance))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.OS))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.ProjectSettings))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.ResourceLoader))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.ResourceSaver))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.FileAccess))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.InputMap))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.MultiplayerApi))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.TranslationServer))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.Mathf))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.PhysicsServer2D))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.PhysicsServer3D))]
+[assembly: Fluentish.InjectableStatic.Injectable(typeof(Godot.RenderingServer))]
